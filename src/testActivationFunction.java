6b4f9ef5de4fe92cdf6d583c3e4a42d2b8bae231
jonster100 <jon_james97@hotmail.com>
2018-09-04 12:07:35 +0100

    Adding a class

---
+++ src/testActivationFunction.java
@@ -0,0 +1,10 @@
+
+public class testActivationFunction implements ActivationFunction {
+
+	@Override
+	public int calculateOutput(int sumWeight,int bias) {
+		// TODO Auto-generated method stub
+		return sumWeight*bias;
+	}
+
+}
